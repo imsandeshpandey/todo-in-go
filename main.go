db2d1f8bf018a3654f4f7e8ab2412e2f5bb2c7c2
Sandesh Pandey <sandeshpandeywork@gmail.com>
2024-10-24 18:30:07 +0545

    📦Initial commit

---
+++ main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"sync"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+func UUID() string {
+	return uuid.New().String()
+}
+
+// region Todos
+type Todo struct {
+	Id        string    `json:"id"`
+	Name      string    `json:"name"`
+	Completed bool      `json:"completed"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+var (
+	todos     []Todo
+	todoMutex sync.Mutex
+)
+
+func todosHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(todos)
+	case "POST":
+		var newTodo Todo
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Unable to read request body", http.StatusInternalServerError)
+			return
+		}
+
+		err = json.Unmarshal(body, &newTodo)
+		if err != nil || newTodo.Name == "" {
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+
+		currTime := time.Now()
+
+		newTodo.Id = UUID()
+		newTodo.CreatedAt = currTime
+		newTodo.UpdatedAt = currTime
+
+		todoMutex.Lock()
+		todos = append(todos, newTodo)
+		todoMutex.Unlock()
+
+		w.WriteHeader(http.StatusCreated)
+		w.Header().Set("Content-Type", "application/json")
+
+		json.NewEncoder(w).Encode(newTodo)
+
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
+}
+
+func todoByIdHandler(w http.ResponseWriter, r *http.Request) {
+	id := r.URL.Path[len("/todos/"):]
+	if id == "" {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+		return
+	}
+
+	todoMutex.Lock()
+	defer todoMutex.Unlock()
+
+	var todoIdx int = -1
+	for idx, todo := range todos {
+		if todo.Id == id {
+			todoIdx = idx
+		}
+	}
+	if todoIdx == -1 {
+		http.Error(w, "Todo not found", http.StatusNotFound)
+		return
+	}
+
+	switch r.Method {
+	case "GET":
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(todos[todoIdx])
+	case "PUT":
+		var updatedTodo Todo
+
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Unable to read request body", http.StatusInternalServerError)
+			return
+		}
+
+		err = json.Unmarshal(body, &updatedTodo)
+		if err != nil || updatedTodo.Name == "" {
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+
+		todos[todoIdx].Name = updatedTodo.Name
+		todos[todoIdx].Completed = updatedTodo.Completed
+		todos[todoIdx].UpdatedAt = time.Now()
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/json")
+
+		json.NewEncoder(w).Encode(todos[todoIdx])
+
+	case "DELETE":
+		todos = append(todos[:todoIdx], todos[todoIdx+1:]...)
+		w.WriteHeader(http.StatusNoContent)
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+	}
+
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+
+	http.HandleFunc("/todos", todosHandler)
+	http.HandleFunc("/todos/", todoByIdHandler)
+
+	http.Handle("/", fileServer)
+	fmt.Println("App is running on port 3000")
+
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		fmt.Println("Error starting server", err)
+	}
+}
---
+++ static/index.js
@@ -0,0 +1,177 @@
+/**
+ *
+ * @typedef {Object} AddTodo
+ * @property {string} name - Name of the task
+ */
+
+/**
+ * @typedef {Object} Todo
+ * @property {string} id
+ * @property {string} name
+ * @property {boolean} completed
+ * @property {Date} created_at
+ * @property {Date} completed_at
+ */
+
+var editingTodoId = "";
+
+/**@type {Map<string, Todo>} */
+const todos = new Map();
+
+var isEditing = false;
+
+const addTodoBtn = document.getElementById("add-todo-btn");
+const todoInput = document.getElementById("todo-input");
+const todosContainer = document.getElementById("todos-container");
+const initialAddTodoBtnContent = addTodoBtn.innerHTML;
+
+//region Event Listeners
+addTodoBtn.addEventListener("click", async () => {
+  const todo = {
+    name: todoInput.value,
+  };
+  addOrEditTodo(todo);
+});
+
+todoInput.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    const todo = {
+      name: todoInput.value,
+    };
+    addOrEditTodo(todo);
+  }
+});
+
+// region Todo Operations
+/**
+ * @returns {Todo[]}
+ */
+async function getTodos() {
+  try {
+    const res = await fetch("./todos");
+    const todos = await res.json();
+    todos.forEach((todo) => addTodoToContainer(todo));
+  } catch {
+    console.log("Error getting todos");
+  }
+}
+
+/** @param {AddTodo} todo */
+function addOrEditTodo(todo) {
+  if (isEditing) return editTodo(todo);
+  return addTodo(todo);
+}
+
+/** @param {AddTodo} todo */
+async function addTodo(todo) {
+  try {
+    const res = await fetch("./todos", {
+      method: "post",
+      body: JSON.stringify(todo),
+    });
+    /** @type {ApiTodo} */
+
+    const newTodo = await res.json();
+    addTodoToContainer(newTodo);
+  } catch {
+    console.log("Error adding todo");
+  }
+}
+
+/** @param {AddTodo} todo */
+async function editTodo(todo) {
+  try {
+    await fetch(`./todos/${editingTodoId}`, {
+      method: "put",
+      body: JSON.stringify(todo),
+    });
+
+    const todoNameEl = document.getElementById(`name.${editingTodoId}`);
+    todoNameEl.innerHTML = todo.name;
+
+    stopEditing();
+  } catch {
+    console.log("Error editing todo");
+  }
+}
+
+async function deleteTodo(id) {
+  try {
+    await fetch(`./todos/${id}`, {
+      method: "delete",
+    });
+    const todoEl = document.getElementById(id);
+    todoEl.remove();
+    todos.delete(id);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function startEditTodo(id) {
+  isEditing = true;
+  editingTodoId = id;
+
+  if (!todos.has(id)) return;
+  const todo = todos.get(id);
+  todoInput.value = todo.name;
+  addTodoBtn.innerHTML = "Update";
+
+  const addTodoRowEl = document.getElementById("add-todo-buttons-row");
+  const cancelButton = `
+    <button id="cancel-edit-button" class="btn btn-ghost" onclick="stopEditing()">
+    Cancel
+    </button>
+  `;
+  addTodoRowEl.insertAdjacentHTML("afterbegin", cancelButton);
+}
+
+function stopEditing() {
+  isEditing = false;
+  addTodoBtn.innerHTML = initialAddTodoBtnContent;
+  todoInput.value = "";
+  const cancelButton = document.getElementById("cancel-edit-button");
+  cancelButton.remove();
+  createLucideIcons();
+}
+
+/**
+ *
+ * @param {Todo} todo
+ */
+function addTodoToContainer(todo) {
+  todos.set(todo.id, todo);
+  const html = `
+        <div class="todo-card" id="${todo.id}" class="flex justify-between items-center gap-2 border rounded-lg px-4 border-white/10 py-2">
+            <p class="todo-name" id="name.${todo.id}">${todo.name}</p>    
+            <div class="todo-action-group">
+            <button id="edit.${todo.id}" class="btn btn-icon btn-ghost">
+                <i data-lucide="pen" class="size-4-icon"></i>
+            </button>
+            <button id="delete.${todo.id}" class="btn btn-ghost btn-delete btn-icon">
+                <i data-lucide="trash" class="size-4-icon"></i>
+            </button>
+            </div>
+        </div>
+        `;
+  todosContainer.insertAdjacentHTML("beforeend", html);
+
+  const deleteButton = document.getElementById(`delete.${todo.id}`);
+  deleteButton.addEventListener("click", () => deleteTodo(todo.id));
+
+  const editButton = document.getElementById(`edit.${todo.id}`);
+  editButton.addEventListener("click", () => startEditTodo(todo.id));
+
+  createLucideIcons();
+}
+
+// region INIT
+getTodos();
+
+function createLucideIcons() {
+  try {
+    lucide.createIcons();
+  } catch (err) {
+    console.log(err);
+  }
+}
